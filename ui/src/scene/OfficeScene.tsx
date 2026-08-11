c9fd8b4589067a7ed6b7035e8b73fb38c1187c0d
rkdehdgml <kdh5677@naver.com>
2026-08-11 16:08:47 +0900

    Switch office camera from isometric perspective to orthographic top-down

--- ui/src/scene/OfficeScene.tsx
+++ ui/src/scene/OfficeScene.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
+import { OrthographicCamera } from "@react-three/drei";
 import { Desk } from "./Desk";
 import { Props } from "./Props";
 import { CharacterActor } from "./CharacterActor";
@@ -21,7 +21,7 @@ export function OfficeScene({
 
   return (
     <Canvas shadows={false}>
-      <PerspectiveCamera makeDefault position={[14, 16, 14]} fov={35} onUpdate={(cam) => cam.lookAt(0, 0, 0)} />
+      <OrthographicCamera makeDefault position={[0, 20, 0.01]} zoom={38} onUpdate={(cam) => cam.lookAt(0, 0, 0)} />
       <ambientLight intensity={0.6} />
       <directionalLight position={[10, 20, 10]} intensity={0.8} />
       <mesh rotation={[-Math.PI / 2, 0, 0]}>
